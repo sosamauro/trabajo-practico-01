8cdaa1c0764a17a8b386a01c0f226dc77148022a
Mauro Sosa <mauro4sosa@gmail.com>
2022-08-11 17:14:24 -0300

    Add project files.

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+	class Programm
+	{
+		static void Main(string[] args)
+		{
+			var numero = 0; 
+			Console.WriteLine("Ingrese un numero: ");
+			numero = Int16.Parse(Console.ReadLine());
+			/*if (numero == 1)
+			{
+				Console.WriteLine("UNO");
+			}
+			else if (numero == 2)
+			{
+				Console.WriteLine("DOS");
+			}
+			else if (numero == 3)
+			{
+				Console.WriteLine("TRES");
+			}
+			else if (numero == 4)
+			{
+				Console.WriteLine("CUATRO");
+			}
+			else if (numero == 5)
+			{
+				Console.WriteLine("CINCO");
+			}
+			else if (numero == 6)
+			{
+				Console.WriteLine("SEIS");
+			}
+			else if (numero == 7)
+			{
+				Console.WriteLine("SIETE");
+			}
+			else if (numero == 8)
+			{
+				Console.WriteLine("OCHO");
+			}
+			else if (numero == 9)
+			{
+				Console.WriteLine("NUEVE");
+			}
+			else
+			{
+				Console.WriteLine("OTRO...");
+			} */
+			switch (numero) 
+			{
+				case 1: Console.WriteLine("UNO");
+				break;
+				case 2: Console.WriteLine("DOS");
+				break;
+				case 3:
+					Console.WriteLine("TRES");
+					break;
+				case 4:
+					Console.WriteLine("CUATRO");
+					break;
+				case 5:
+					Console.WriteLine("CINCO");
+					break;
+				case 6:
+					Console.WriteLine("SEIS");
+					break;
+				case 7:
+					Console.WriteLine("SIETE");
+					break;
+				case 8:
+					Console.WriteLine("OCHO");
+					break;
+				case 9:
+					Console.WriteLine("NUEVE");
+					break;
+				default : Console.WriteLine("OTRO");
+					break;
+			}
+
+		}
+	}
+}
---
+++ ejercicio2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var limiteInferior = 0;
+			var limiteSuperior = 0;
+			var suma = 0;
+			var promedio = 0.0;
+
+			Console.WriteLine("Ingrese limite inferior: ");
+			limiteInferior = Int16.Parse(Console.ReadLine());
+			Console.WriteLine("Ingrese limite superior: ");
+			limiteSuperior = Int16.Parse(Console.ReadLine());
+
+			suma = limiteInferior;
+
+			/*for (int i = limiteInferior + 1; i <= limiteSuperior; i++) 
+			{
+				suma = suma + i;
+			}*/
+			int i = limiteInferior;
+			while (i < limiteSuperior)
+			{
+				i++;
+				suma = suma + i;
+			}
+
+			promedio = (limiteInferior + limiteSuperior) / 2;
+			Console.WriteLine("suma: " + suma);
+			Console.WriteLine("promedio: " + promedio);
+			Console.WriteLine("maximo: " + limiteSuperior);
+			Console.WriteLine("minimo: " + limiteInferior);
+
+
+		}
+	}
+}
