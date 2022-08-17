584e97a649e213c55f637603e0b955db25094de0
Mauro Sosa <mauro4sosa@gmail.com>
2022-08-17 18:29:24 -0300

    subo ejercicio 8

--- ejercicio8/Program.cs
+++ ejercicio8/Program.cs
@@ -10,46 +10,23 @@ namespace ejercicio8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("inserta en número el nivel de la pirámide, mayor a cero");
+            var tamaño = 8;
+            var cadena = "";
 
-            int N = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= N; i++)
+            for (var i = 1; i <= tamaño; i++)
             {
-                Console.WriteLine(" \t ");
-                //primera parte
-                for (int j = 1; j <= (N - i) + 1; j++)
+                var c = 1;
+                cadena = "";
+                for (int j = 1; j <= i; j++)
                 {
-                    Console.Write("* ");
+                    cadena = cadena + " " + c;
+                    c = c * (i - j) / j;
                 }
-                //segunda parte
-                int numeroFila = i;
-                Boolean bandera = true;
-                for (int j = 1; j <= (i * 2) - 1; j++)
-                {
-                    Console.Write(numeroFila + " ");
-                    if (bandera)
-                    {
-                        numeroFila--;
-                        if (numeroFila == 1)
-                        {
-                            bandera = false;
-                        }
-                        continue;
-                    }
-                    numeroFila++;
-                    //else
-                    //{
-                    //    numeroFila ++;
-                    //}
-                }
-                for (int j = 1; j <= (N - i) + 1; j++)
-                {
-                    Console.Write("* ");
-                }
-
-                Console.WriteLine(" ");
-
+                Console.WriteLine(cadena);
             }
+
+
+
         }
     }
 }
